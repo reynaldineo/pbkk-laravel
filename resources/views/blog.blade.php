e631d9f9fcccf6adcf8e9cf1a3c91ae6a4c109d5
reynaldi neo <reynaldineoramadhani@gmail.com>
2024-09-11 23:37:21 +0700

    feat: add blade template engine & alpine js & tailwind ui

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Blog</title>
+</head>
+<body>
+    <h1>Ini Halaman Blog</h1>
+
+    <h2>Ini Sub Blog</h2>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse, at.</p>
+
+    <h2>Ini Sub Blog</h2>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse, at.</p>
+</body>
+</html>
