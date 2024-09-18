507f7dd2001a49726420ceded9825dd4ab69e821
reynaldi neo <reynaldineoramadhani@gmail.com>
2024-09-18 23:33:18 +0700

    feat: pass post data to view

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,49 @@ Route::get('/about', function () {
     return view('about', ['title' => 'About', 'name' => 'Rey']);
 });
 
-Route::get('/blog', function () {
-    return view('blog', ['title' => 'Blog']);
+Route::get('/posts', function () {
+    return view('posts', ['title' => 'Blog', 'posts' => [
+        [
+            'id' => '1',
+            'slug'=> 'judul-artikel-1',
+            'title' => 'Judul Artikel 1',
+            'author' => 'Reynaldi Neo',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime cum illo adipisci, aliquam esse aliquid ipsam quod laboriosam distinctio voluptate! Iste ad animi itaque eaque id explicabo, esse quidem porro.'
+        ],
+        [   
+            'id'=> '2',
+            'slug'=> 'judul-artikel-2',
+            'title' => 'Judul Artikel 2',
+            'author' => 'Reynaldi Neo',
+            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid deleniti recusandae quos fugiat error labore at maiores consequatur, earum reiciendis alias sequi ipsum, ad inventore esse minus vel qui! Nulla.'
+        ]
+    ]]);
 }); 
+
+Route::get('/posts/{slug}', function ($slug){
+    $posts = [
+        [
+            'id' => '1',
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul Artikel 1',
+            'author' => 'Reynaldi Neo',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime cum illo adipisci, aliquam esse aliquid ipsam quod laboriosam distinctio voluptate! Iste ad animi itaque eaque id explicabo, esse quidem porro.'
+        ],
+        [   
+            'id'=> '2',
+            'slug'=> 'judul-artikel-2',
+            'title' => 'Judul Artikel 2',
+            'author' => 'Reynaldi Neo',
+            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid deleniti recusandae quos fugiat error labore at maiores consequatur, earum reiciendis alias sequi ipsum, ad inventore esse minus vel qui! Nulla.'
+        ]
+    ];
+
+    $post = Arr::first($posts, function($post) use ($slug){
+        return $post['slug'] == $slug;
+    });
+
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
+});
 
 Route::get('/contact', function () {
     return view('contact', ['title' => 'Contact']);
